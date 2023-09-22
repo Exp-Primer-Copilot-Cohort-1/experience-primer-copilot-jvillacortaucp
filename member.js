0c86fceecc3937ca190b171a566dcf8ac084970d
José Miguel Villacorta Arteaga <139243483+jvillacortaucp@users.noreply.github.com>
2023-09-22 10:49:42 +0000

    member 22-09

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return {
+        name: "John",
+        age: 30,
+        skills: ["HTM", "CSS", "JS"],
+        address: {
+            city: "Calicut",
+            pincode: 673001
+        },
+        dateOfBirth: new Date('1990-01-01'),
+        married: true,
+        profession: "Analyst"
+    };
+}
